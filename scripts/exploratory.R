785a5e1486c3e26b1b0be29cd91246ac39f4c20b
Ronja <ronja@hegelbach.com>
2026-04-27 22:26:37 +0200

    restructure files, move scripts into new folders

--- scripts/exploratory.R
+++
@@ -1,9 +0,0 @@
-renv::init()
-
-library(jsonlite)
-
-rawdata <- fromJSON("swiss_nowcast_metadata/swiss_nowcast_data.json", 
-                    flatten = TRUE)
-head(rawdata)
-names(rawdata)
-summary(rawdata)
